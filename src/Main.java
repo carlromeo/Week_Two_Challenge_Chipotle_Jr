0e4f4f2cfd61089620d79ae492e43eb2c1dff693
carlromeo <carlromeo.dogbey@gmail.com>
2019-09-12 17:48:49 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[]args) {
+
+
+        String[] rice = {"white","brown","no rice"};
+//        String randomString =" ";
+        Random random = new Random();
+
+        for (int i = 0; i< rice.length; i++) {
+
+            int random  = random.nextInt(3);
+        }
+
+       }
+    }
+
