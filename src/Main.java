3657cd243281483f6a4234aa91ac7c2a18928577
Carl Romeo Dogbey <carlromeo.dogbey@gmail.com>
2019-09-13 07:25:37 -0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,40 @@ public class Main {
     public static void main(String[]args) {
 
 
+        System.out.println("");
+
         String[] rice = {"white","brown","no rice"};
-//        String randomString =" ";
-        Random random = new Random();
+        String[] meat = {"chicken","steak", "carnidas", "chorizo","sofritas","veggies"};
+        String[] beans= {"pinto", "black", " no beans"};
+        String[] salsa = {"mild salsa","medium salsa", "hot salsa", "no salsa"};
+        String[] veggies = {"lettuce","fajita veggies","no veggies"};
 
-        for (int i = 0; i< rice.length; i++) {
+        Random riceOrder = new Random();
+        Random meatOrder = new Random();
+        Random beansOrder = new Random();
+        Random salsaOrder = new Random();
+        Random veggiesOrder = new Random();
 
-            int random  = random.nextInt(3);
-        }
+        int indexRice  = riceOrder.nextInt(rice.length );
+        int indexMeat  = meatOrder.nextInt(meat.length);
+        int indexBeans = beansOrder.nextInt(beans.length);
+        int indexSalsa = salsaOrder.nextInt(beans.length);
+        int indexVeggies = veggiesOrder.nextInt(beans.length);
+
+            System.out.print("Burito 1: "
+                    + rice[indexRice] + ", "
+                    + meat[indexMeat] + ", "
+                    + beans[indexBeans]+ ", "
+                    + salsa[indexSalsa]+ ", "
+                    + veggies[indexVeggies]+ ".");
+
+
+
+
+//        }
 
        }
     }
+
+
 
